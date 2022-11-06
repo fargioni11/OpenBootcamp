8b5473f5290c1188fa5c12acbf2e10423c714d9b
Adriel Fargioni <adrielfargioni@gmail.com>
2022-11-06 09:52:09 -0300

    first commit

---
+++ ejercicio-01.js
@@ -0,0 +1,13 @@
+// Array o arreglos
+const infoPersonal = [
+  "adriel",
+  33,
+  true,
+  new Date("1989-07-12"),
+  (libFav = {
+    title: "Don Quijote",
+    Author: "Cervantes",
+    publication: 1889,
+    info: "https://es.wikipedia.org/wiki/Don_Quijote_de_la_Mancha",
+  }),
+];
